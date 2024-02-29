b96cca9b24a58684d9c733725ace28e7ef9568e5
Jason Amundson <jmamundson@alaska.edu>
2024-02-29 11:10:54 +0200

    Modified mesh to have inflow boundary.

--- mesh/s1_make_geo_and_mesh.sh
+++ mesh/s1_make_geo_and_mesh.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# first run conda activate glaciome to use an environment that has shapefile installed
-# once start making physical lines and surface, the numbering matters
-if test -f "taku.geo"; then
-    rm taku.geo
-fi 
+# if test -f "taku.geo"; then
+#    rm taku.geo
+# fi 
 
-source activate glaciome
-python Contour2geo.py -r 500 -i outline/taku_outline.shp -o taku.geo # convert shape file into geo file
-conda deactivate
+# source activate glaciome
+# python Contour2geo.py -r 200 -i outline/taku_outline.shp -o taku.geo # convert shape file into geo file
+# conda deactivate
+
+# Important: need to edit geo file manually so that the inflow boundary is separated from the rest of the boundary.
 
 gmsh taku.geo -2
-
-# need to edit mesh since we one the upper boundary to have a different boundary condition than the rest of the outline
 
 # ElmerGrid 14 2 mendenhall.msh -autoclean
 # ElmerGrid 14 5 mendenhall.msh -autoclean
